5666f1c0ea2b28fa3382860b09a3370f0a6fbda5
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2018-01-16 23:12:41 -0400

    print player plays on board

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var player = {
+var Player = {
   symbol: '',
   set: function(symbol){
     if(this.symbol === '')
@@ -9,75 +9,103 @@ var player = {
   }
 };
 
+var player1 = Object.create(Player), 
+computer = Object.create(Player),
+playerOnTurn;
+
+// [y][x]
+var board = [["","",""],
+["","",""],
+["","",""]];
+
 
 $(document).ready(function(){
 
   // Player selection modal configuration
   $('#playerSelectionModal').modal({
-      dismissible: false,
-      opacity: .5,
-      inDuration: 200,
-      outDuration: 400,
-      startingTop: '4%',
-      endingTop: '40%',
-      ready: function(modal) {
+    dismissible: false,
+    opacity: .5,
+    inDuration: 200,
+    outDuration: 400,
+    startingTop: '4%',
+    endingTop: '40%',
+    ready: function(modal) {
         // Sets modal on the center of the screen vertically
         modal.css('transform', 'translateY(-40%)');        
       }
     }
-  );
+    );
   $('#playerSelectionModal').modal('open');
 });
+
+
+function playOnPosition(pos, player){
+  
+  if(board[pos.y][pos.x] == ""){
+    board[pos.y][pos.x] = player.get();
+    $('#' + pos.y + pos.x).text(playerOnTurn.get());
+    
+    // changing turns
+    if(playerOnTurn === player1){
+      playerOnTurn = computer;
+    } else {
+      playerOnTurn = player1;
+    }
+  }
+}
 
 
 // Player X selection on modal event
 $('#player-x').on('click', function(){
   $('#player1').text('Computer');
   $('#player2').text('Player 1');
-  player.set('X');
+  player1.set('X');
+  computer.set('O');
+  playerOnTurn = player1;
 });
 
 // Player O selection on modal event
 $('#player-o').on('click', function(){
   $('#player1').text('Player 1');
   $('#player2').text('Computer');
-  player.set('O');
+  player1.set('O');
+  computer.set('X');
+  playerOnTurn = player1;
 });
 
 
-
-$('#0').on('click', function(){
-  console.log("0,0");
+$('#00').on('click', function(){
+  playOnPosition({y: 0, x: 0}, playerOnTurn);
 });
 
-$('#1').on('click', function(){
-  console.log("1,0");
+$('#01').on('click', function(){
+  playOnPosition({y: 0, x: 1}, playerOnTurn);
 });
 
-$('#2').on('click', function(){
-  console.log("2,0");
+$('#02').on('click', function(){
+  playOnPosition({y: 0, x: 2}, playerOnTurn);
 });
 
-$('#3').on('click', function(){
-  console.log("0,1");
+$('#10').on('click', function(){
+  playOnPosition({y: 1, x: 0}, playerOnTurn);
 });
 
-$('#4').on('click', function(){
-  console.log("1,1");
+$('#11').on('click', function(){
+  playOnPosition({y: 1, x: 1}, playerOnTurn);
 });
 
-$('#5').on('click', function(){
-  console.log("2,1");
+$('#12').on('click', function(){
+  playOnPosition({y: 1, x: 2}, playerOnTurn);
 });
 
-$('#6').on('click', function(){
-  console.log("0,2");
+$('#20').on('click', function(){
+  playOnPosition({y: 2, x:0 }, playerOnTurn);
 });
 
-$('#7').on('click', function(){
-  console.log("1,2");
+$('#21').on('click', function(){
+  playOnPosition({y: 2, x: 1}, playerOnTurn);
 });
 
-$('#8').on('click', function(){
-  console.log("2,2");
+$('#22').on('click', function(){
+  playOnPosition({y: 2, x: 2}, playerOnTurn);
 });
